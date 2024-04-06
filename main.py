1ff7909166958803e0e571382e37a5cac6846559
pamquale <pamquale@gmail.com>
2024-04-06 16:58:13 +0300

    all the trash 2

--- main.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import csv
-
-df = pd.read_csv('dataset1_updated.csv', header=None)  # Load CSV (assuming no header)
-df.to_csv('reformatted_file2.csv', index=False, header=False, quoting=csv.QUOTE_ALL)
--- sort.py
+++ sort.py
@@ -1,14 +1,21 @@
 import pandas as pd
+import csv
 
 # Load datasets into dataframes
 df1 = pd.read_csv('data/comp_datasets_train/solution_format.csv')
-df2 = pd.read_csv('reformatted_file.csv')
+df2 = pd.read_csv('newshi/region_A_test.csv')
 
-# Merge dataframes on 'date' column
-df_merged = df1.merge(df2, on='date', suffixes=('_old', ''))
+# Create a dictionary where key is the date and value is the order in df2
+order_dict = {k: v for v, k in enumerate(df2['date'])}
 
-# Overwrite 'label' in df1 with updated values
-df1['label'] = df_merged['label']
+# Create a new column in df1 called 'order' using the 'date' column mapped to the order_dict
+df1['order'] = df1['date'].map(order_dict)
+
+# Sort df1 by this 'order' column
+df1.sort_values('order', inplace=True)
+
+# Drop the 'order' column as it's not needed anymore
+df1.drop('order', axis=1, inplace=True)
 
 # Save the updated Dataset 1
-df1.to_csv('dataset1_updated.csv', index=False)
+df1.to_csv('data/comp_datasets_train/solution_format.csv', index=False, quoting=csv.QUOTE_ALL)
